24e74d8cd9a94342baeddda893188675ec92a161
Mark Haskamp <mark.haskamp@pinnsg.com>
2011-08-22 17:50:04 -0400

    saved lunch spots are displayed alphabetically

--- app/public/javascripts/SavedLunchSpots.js
+++ app/public/javascripts/SavedLunchSpots.js
@@ -13,11 +13,13 @@ var SavedLunchSpotsView = {
 
     if (lunch_spot_list !== null) {
 
-      html_val = $('#saved_lunch_spots').val();
+      // var html_val = $('#saved_lunch_spots').val();
+      var html_val = '';
 
-      $.each(lunch_spot_list, function(ndx, spot) {
-        var div_line = '<div><span class="lunch_spot">' + spot + '</span> &nbsp; <span class="vote_for_saved cursor_hover">+1</span> &nbsp; <span class="delete_saved cursor_hover">X</span></div>';
-        html_val += div_line
+      $.each(lunch_spot_list.sort(function(a,b) { return(a > b); }), 
+        function(ndx, spot) {
+          var div_line = '<div><span class="lunch_spot">' + spot + '</span> &nbsp; <span class="vote_for_saved cursor_hover">+1</span> &nbsp; <span class="delete_saved cursor_hover">X</span></div>';
+          html_val += div_line
       });
 
       $('#saved_lunch_spots').html(html_val);
