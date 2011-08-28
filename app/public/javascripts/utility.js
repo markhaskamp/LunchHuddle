05888dedcbaac075ed0975a3acd5eabab44cc5b8
Mark Haskamp <mark.haskamp@pinnsg.com>
2011-08-28 15:52:36 -0400

    clear localstorage from utility page

--- app/public/javascripts/utility.js
+++ app/public/javascripts/utility.js
@@ -4,5 +4,9 @@ $(document).ready(function() {
     local_storage_str = "true";
   }
   $('#local_storage').html(local_storage_str);
+
+  $('#clear_local_storage').click( function() { localStorage.removeItem('lh_lunch_spots'); });
+
+
 });
 
