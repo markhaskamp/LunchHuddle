0ef277d89ead0ea4c2ce09f7df144cee20e2155a
Mark Haskamp <mark.haskamp@pinnsg.com>
2011-08-29 21:07:49 -0400

    refactoring for UserInfo

--- app/public/javascripts/UserInfo.js
+++ app/public/javascripts/UserInfo.js
@@ -2,6 +2,9 @@ var UserInfoView = {
 
   get_name: function() {
     return($('#txtName').val());
-  }
+  },
 
+  disable_user_name: function() {
+    $('#txtName').attr('disabled', 'disabled');
+  }
 }
