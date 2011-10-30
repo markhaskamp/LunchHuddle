d71bbf5321e51c297abd2b99078ba097d6a2ad5f
Mark Haskamp <mark.haskamp@pinnsg.com>
2011-10-30 15:05:04 -0400

    working on ActionMailer and sendgrid

--- app/webstart.rb
+++ app/webstart.rb
@@ -1,6 +1,27 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'action_mailer'
+
+ActionMailer::Base.smtp_settings = {
+  :address => "smtp.sendgrid.net",
+  :port => '25',
+  :domain => ENV['SENDGRID_DOMAIN'],
+  :authentication => :plain,
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD']
+}
+
+class MyMailer < ActionMailer::Base
+ def email
+         mail(
+          :to         => "mrhask@gmail.com",
+          :from       => "app760353@heroku.com",
+          :subject    => "test email",
+          :body       => "test email from lunchhuddle via heroke/sendgrid"
+         )
+  end
+end
 
 get '/' do
   @huddle = params[:huddle]
@@ -27,5 +48,10 @@ get '/utility' do
   @huddle = params[:huddle]
   @huddle ||= 'lunch_huddle'
   haml :utility
+end
+
+get '/huddle/invite' do
+  MyMailer.email.deliver
+  'eddie would go'
 end
 
