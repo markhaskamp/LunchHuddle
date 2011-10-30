f1110d4beb76ec492ba8d3bd26afc0aabbabc87e
Mark Haskamp <mark.haskamp@pinnsg.com>
2011-10-30 12:02:24 -0400

    adding name and email address as an Invitee object to Invitee list

--- app/public/javascripts/view/AddInviteeView.js
+++ app/public/javascripts/view/AddInviteeView.js
@@ -5,11 +5,15 @@ var AddInviteeView = Backbone.View.extend({
   }
 
   , on_btnAdd_click: function() {
-    invitee_list_view.trigger('add_invitee', this.get_email_addr());
+    invitee_list_view.trigger('add_invitee', this.get_name(),  this.get_email_addr());
   }
 
   , get_email_addr: function() {
     return($('#txtEmailAddr').val());
+  }
+
+  , get_name:function() {
+    return($('#txtName').val());
   }
 
 });
--- app/public/javascripts/view/InviteeListView.js
+++ app/public/javascripts/view/InviteeListView.js
@@ -12,16 +12,19 @@ var InviteeListView = Backbone.View.extend({
     _.each(this.invitees, function(invitee) {
       html_str += 
             '<div class="invitee_item">' +
-              '<span class="invitee_item_name">' + 'foo' + '</span>' + 
-              '<span class="invitee_item_email">' + invitee + '</span>' +
+              '<span class="invitee_item_name">' + invitee.name + '</span>' + 
+              '<span class="invitee_item_email">' + invitee.email_addr + '</span>' +
             '</div>';
     });
     $('#invitee_list').html(html_str);
   }
 
-  , on_add_invitee: function(email_addr) {
-    this.invitees.push(email_addr);
+  , on_add_invitee: function(name, email_addr) {
+    var new_invitee = Object.beget(Invitee);
+    new_invitee.name = name;
+    new_invitee.email_addr = email_addr;
 
+    this.invitees.push(new_invitee);
     this.render();
   }
 
