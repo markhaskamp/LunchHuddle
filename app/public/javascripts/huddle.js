e99eedd0331774d78c568f8e912d0fffd60f51d8
Mark Haskamp <mark.haskamp@pinnsg.com>
2011-10-30 11:33:19 -0400

    adding email addresses to the invitee list

--- app/public/javascripts/huddle.js
+++ app/public/javascripts/huddle.js
@@ -3,8 +3,8 @@ var invitee_list_view
 
 $(document).ready(function() {
 
-  add_form_view = new AddInviteeView({"el": $("#frmAdd")});
-  invitee_list_view = new InviteeListView({"el": $("#invitee_list")});
+  add_form_view = new AddInviteeView({"el": $("#add_invitee_view")});
+  invitee_list_view = new InviteeListView({"el": $("#invitee_list_view")});
 
   _.extend(invitee_list_view, Backbone.Events);
   invitee_list_view.bind('add_invitee', invitee_list_view.on_add_invitee);  // move to view init?
--- app/public/javascripts/view/InviteeListView.js
+++ app/public/javascripts/view/InviteeListView.js
@@ -1,12 +1,24 @@
 var InviteeListView = Backbone.View.extend({
 
-  invitees: [],
+  invitees: []
 
-  events: {
-  },
+  , events: {
+  }
 
-  on_add_invitee: function(email_addr) {
-    alert(email_addr);
+  , render: function() {
+    $('#invitee_list').html('');
+    var html_str = '';
+
+    _.each(this.invitees, function(invitee) {
+      html_str += '<span class="invitee_item">' + invitee + '</span><br />';
+    });
+    $('#invitee_list').html(html_str);
+  }
+
+  , on_add_invitee: function(email_addr) {
+    this.invitees.push(email_addr);
+
+    this.render();
   }
 
 });
