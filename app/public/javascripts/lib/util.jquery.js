76830afd576a7ed2dc6c44083b5a18dacaa95316
Mark Haskamp <mark.haskamp@pinnsg.com>
2011-12-17 20:46:15 -0500

    refactor

--- app/public/javascripts/lib/util.jquery.js
+++ app/public/javascripts/lib/util.jquery.js
@@ -20,8 +20,6 @@ function type(obj){
 }
 
 
-// $('.cursor_hover').hover( function() { $(this).css('cursor', 'pointer'); },
-//                           function() { $(this).css('cursor', 'default'); });
 $('.cursor_hover').live('mouseover', function() { $(this).css('cursor', 'pointer'); });
 $('.cursor_hover').live('mouseout', function() { $(this).css('cursor', 'default'); });
 
